fe5ba102c5f6c0804eee568dec0527aa35ab76ca
Olabisi Olukoga <bisikoga@gmail.com>
2026-03-24 15:59:46 +0000

    Add numbers.py file

---
+++ numbers.py
@@ -0,0 +1,14 @@
+# Numbers and arithmetic operations
+print(5 + 3) # Addition
+print(10 - 4)  # Subtraction
+print(6 * 7)  # Multiplication
+print(15 / 3)  # Division
+print(2 ** 8)  # Exponentiation
+print(10 % 3)  # Modulus
+print(10 // 3)  # Floor division
+# Order of operations
+print(2 + 3 * 4)  # Multiplication before addition
+print((2 + 3) * 4)  # Parentheses change the order
+
+
+
